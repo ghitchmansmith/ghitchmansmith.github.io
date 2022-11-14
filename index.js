cb9251b5fe45ce3a34f5e248da535ad1fa121a7a
ghitchmansmith <114439927+ghitchmansmith@users.noreply.github.com>
2022-11-14 14:35:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function sendNameToOS(){
-    var fName = document.getElementById("first_name");
-    var lName = document.getElementById("last_name");
+    var fName = document.getElementById("first_name").value;
+    var lName = document.getElementById("last_name").value;
 
     OneSignal.push(function(){
         OneSignal.sendTags({"first_name": fName, "last_name": lName,});
