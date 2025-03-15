2251e0787de1def0d881407a173fe17052135c64
ghitchmansmith <114439927+ghitchmansmith@users.noreply.github.com>
2025-03-15 13:43:31 +0000

    add wave height

--- surf_app_test/script.js
+++ surf_app_test/script.js
@@ -62,12 +62,13 @@ document.addEventListener("DOMContentLoaded", async () => {
         tr.innerHTML = `
             <td>${new Date(row.time).toLocaleString()}</td>
             <td>${row.surf_score_readable}</td>
+            <td>${row.wave_height}s</td>
+            <td>${row.wave_period}s</td>
             <td>${row.swell_direction}°</td>
             <td class="${row.swell_height < 0.4 ? "cell-red" : ""}">${
           row.swell_height
         }m</td>
             <td>${row.swell_period}s</td>
-            <td>${row.wave_period}s</td>
             <td>${row.wind_direction}°</td>
             <td>${row.wind_speed} m/s</td>
           `;
